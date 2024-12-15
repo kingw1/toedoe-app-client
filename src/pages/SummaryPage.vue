a4f80fbbb3f5146e84abec5cb18248210b6730c7
King <wiwichai@gmail.com>
2024-12-15 21:13:53 +0700

    summary component

--- src/pages/SummaryPage.vue
+++ src/pages/SummaryPage.vue
@@ -1,3 +1,41 @@
 <template>
-    <h1>Summary</h1>
+    <main style="min-height: 50vh; margin-top: 2rem">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 offset-md-2">
+                    <div
+                        class="d-flex justify-content-between align-items-center mb-4"
+                    >
+                        <h1>
+                            Summary
+                            <small class="text-muted">(Tasks this week)</small>
+                        </h1>
+                        <SummaryFilter />
+                    </div>
+                    <div
+                        v-for="(tasks, description) in summaries"
+                        :key="description"
+                    >
+                        <Summaries :tasks="tasks" :description="description" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
 </template>
+
+<script setup>
+import { useSummaryStore } from "../stores/summary";
+import { onMounted } from "vue";
+import { storeToRefs } from "pinia";
+import Summaries from "@/components/summaries/Summaries.vue";
+import SummaryFilter from "@/components/summaries/filter/SummaryFilter.vue";
+
+const store = useSummaryStore();
+const { summaries } = storeToRefs(store);
+const { fetchTasksSummary } = store;
+
+onMounted(async () => {
+    await fetchTasksSummary();
+});
+</script>
