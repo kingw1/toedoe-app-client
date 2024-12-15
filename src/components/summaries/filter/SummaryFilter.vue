19d6729462406b5eaceb3edbb4acce7fa32be4c6
King <wiwichai@gmail.com>
2024-12-15 21:19:29 +0700

    summary filter component

---
+++ src/components/summaries/filter/SummaryFilter.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { computed } from "vue";
+import Dropdown from "@/components/dropdown/Dropdown.vue";
+import DropdownItem from "@/components/dropdown/DropdownItem.vue";
+import DropdownTrigger from "@/components/dropdown/DropdownTrigger.vue";
+import { endOfWeek, format, startOfWeek, subWeeks } from "date-fns";
+
+const filterItems = computed(() => {
+    const thisWeekStart = format(startOfWeek(new Date()), "d MMM");
+    const thisWeekEnd = format(endOfWeek(new Date()), "d MMM");
+    const lastWeekStart = format(startOfWeek(subWeeks(new Date(), 1)), "d MMM");
+    const lastWeekEnd = format(endOfWeek(subWeeks(new Date(), 1)), "d MMM");
+
+    return {
+        today: "Today",
+        yesterday: "Yesterday",
+        thisweek: `This week (${thisWeekStart} - ${thisWeekEnd})`,
+        lastweek: `Last week (${lastWeekStart} - ${lastWeekEnd})`,
+        thismonth: "This month",
+        lastmonth: "Last month",
+    };
+});
+</script>
+
+<template>
+    <Dropdown>
+        <template v-slot:trigger="{ toggleClass, toggle }">
+            <DropdownTrigger
+                class="btn btn-outline-secondary"
+                :class="toggleClass"
+                type="button"
+                @click="toggle"
+                >Filter</DropdownTrigger
+            >
+        </template>
+        <template v-slot:menu="{ toggle }">
+            <DropdownItem
+                v-for="(value, key) in filterItems"
+                :key="key"
+                href="#"
+                @click.prevent="toggle"
+                >{{ value }}</DropdownItem
+            >
+        </template>
+    </Dropdown>
+</template>
