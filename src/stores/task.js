7029b89f329f2b404045ee08b2d20ce4a2703c1f
King <wiwichai@gmail.com>
2024-12-15 15:40:33 +0700

    refactore task component to pinia store

--- src/stores/task.js
+++ src/stores/task.js
@@ -1,6 +1,12 @@
 import { defineStore } from "pinia";
-import { allTasks } from "../http/task-api";
-import { computed, reactive, ref } from "vue";
+import {
+    allTasks,
+    completeTask,
+    createTask,
+    removeTask,
+    updateTask,
+} from "../http/task-api";
+import { computed, ref } from "vue";
 
 export const useTaskStore = defineStore("taskStore", () => {
     const tasks = ref([]);
@@ -17,10 +23,41 @@ export const useTaskStore = defineStore("taskStore", () => {
         tasks.value = data.data;
     };
 
+    const handleAddedTask = async (newTask) => {
+        const { data: createdTask } = await createTask(newTask);
+        tasks.value.unshift(createdTask.data);
+    };
+
+    const handleUpdatedTask = async (task) => {
+        const { data: updatedTask } = await updateTask(task.id, {
+            name: task.name,
+        });
+        const currentTask = tasks.value.find((item) => item.id === task.id);
+        currentTask.name = updatedTask.data.name;
+    };
+
+    const handleCompletedTask = async (task) => {
+        const { data: updatedTask } = await completeTask(task.id, {
+            is_completed: task.is_completed,
+        });
+        const currentTask = tasks.value.find((item) => item.id === task.id);
+        currentTask.is_completed = updatedTask.data.is_completed;
+    };
+
+    const handleRemovedTask = async (task) => {
+        await removeTask(task.id);
+        const index = tasks.value.findIndex((item) => item.id === task.id);
+        tasks.value.splice(index, 1);
+    };
+
     return {
         tasks,
         completedTasks,
         uncompletedTasks,
         fetchAllTasks,
+        handleAddedTask,
+        handleUpdatedTask,
+        handleCompletedTask,
+        handleRemovedTask,
     };
 });
